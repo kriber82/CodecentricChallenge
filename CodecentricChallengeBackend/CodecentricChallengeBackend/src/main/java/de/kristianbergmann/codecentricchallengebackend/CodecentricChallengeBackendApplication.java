e8479bd54cff23ff1bd4dda85fcb44f1ca54a840
kriber82 <31168700+kriber82@users.noreply.github.com>
2024-02-20 11:26:05 +0100

    Created backend project

---
+++ CodecentricChallengeBackend/CodecentricChallengeBackend/src/main/java/de/kristianbergmann/codecentricchallengebackend/CodecentricChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package de.kristianbergmann.codecentricchallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecentricChallengeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecentricChallengeBackendApplication.class, args);
+    }
+
+}
---
+++ CodecentricChallengeBackend/CodecentricChallengeBackend/src/test/java/de/kristianbergmann/codecentricchallengebackend/CodecentricChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kristianbergmann.codecentricchallengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecentricChallengeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
